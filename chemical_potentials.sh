48da16a58e19e34d336cbf23b02bb779b04518e6
Jacob Jeffries <jwjeffr@login003.palmetto.clemson.edu>
2024-02-02 23:07:00 -0500

    initial commit

---
+++ chemical_potentials.sh
@@ -0,0 +1,44 @@
+config_file_name=$1
+
+cfg () {
+	jq -r $1 ${config_file_name}
+}
+
+lmp=$(cfg '.exec')
+np=$(cfg '.np')
+units=$(cfg '.units')
+input_data_file=$(cfg '.input_data')
+
+pair_style=$(cfg '.pair_style')
+pair_coeff=$(cfg '.pair_coeff')
+
+potential_file=$(cfg '.potential_file')
+echo "pair_style ${pair_style}" > ${potential_file}
+echo "pair_coeff ${pair_coeff}" >> ${potential_file}
+
+dmax=$(cfg '.dmax')
+pressure=$(cfg '.pressure')
+vmax=$(cfg '.vmax')
+etol=$(cfg '.etol')
+ftol=$(cfg '.ftol')
+maxsteps=$(cfg '.maxsteps')
+
+enthalpy_file=$(cfg '.enthalpy_file')
+ntypes=$(grep -oP '\d+(?=\s*atom types)' ${input_data_file})
+occupying_energies_file=$(cfg '.occupying_energies')
+
+log_file=$(cfg '.log_file')
+
+mpirun -np ${np} ${lmp} -in in.insertions \
+  -var units ${units} \
+  -var input_data_file ${input_data_file} \
+  -var potential_file ${potential_file} \
+  -var dmax ${dmax} \
+  -var pressure ${pressure} \
+  -var vmax ${vmax} \
+  -var etol ${etol} \
+  -var ftol ${ftol} \
+  -var maxsteps ${maxsteps} \
+  -var enthalpy_file ${enthalpy_file} \
+  -var occupying_energies_file ${occupying_energies_file} \
+  -log ${log_file}
