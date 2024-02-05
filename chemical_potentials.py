ce8b51bb492330ad593995c236b0956d43bfde8d
Jacob Jeffries <jwjeffr@node1403.palmetto.clemson.edu>
2024-02-05 17:24:20 -0500

    added comments to shell and python script

--- chemical_potentials.py
+++ chemical_potentials.py
@@ -1,3 +1,7 @@
+"""
+module for performing chemical potential calculations
+"""
+
 import sys
 from itertools import combinations
 from typing import IO, Tuple
@@ -12,27 +16,54 @@ from numpy.lib.stride_tricks import sliding_window_view
 @dataclass
 class Filter(ABC):
 
+    """
+    abstract base class for a filter, useful for creating domains
+    """
+
     @abstractmethod
     def employ(self, data: ArrayLike) -> Tuple[ArrayLike, float]:
+    
+        """
+        abstract method for employing the filter, returns new data and p(ω)
+        """
+    
         pass
         
         
 @dataclass
 class IdentityFilter(Filter):
 
+    """
+    identity filter, does nothing
+    """
+
     def employ(self, data: ArrayLike) -> Tuple[ArrayLike, float]:
+    
+        """
+        return original data and p(Ω) = 1
+        """
+
         return data, 1.0
         
         
 @dataclass
 class OneDimChunkFilter(Filter):
 
+    """
+    filter data such that we create a chunk over the 1d interval [low, high]
+    """
+
     axis: int
     low: float
     high: float
 
-    def employ(self, data: ArrayLike) -> ArrayLike:
+    def employ(self, data: ArrayLike) -> ArrayLike
     
+        """
+        filter data, return data captured in interval [low, high] and p(ω)
+        """
+    
+        # mask data over the interval
         mask = np.logical_and(self.low <= data[:, -3 + self.axis], data[:, -3 + self.axis] <= self.high)
         new_data = data[mask]
         
@@ -41,35 +72,52 @@ class OneDimChunkFilter(Filter):
 
 def get_chemical_potentials(data: ArrayLike, filter_: callable = IdentityFilter()) -> NDArray:
 
+    """
+    function for getting chemical potentials
+    """
+
+    # apply filter, defaults to doing nothing if filter not specified
     data, p = filter_.employ(data)
 
     num_types = data.shape[1] - 4
     
     init_types = data[:, 0].astype(int)
+    
+    # grab occupying energies, rescale by p(ω)
     occupying_energies = data[:, 1:num_types + 1]
     occupying_energies *= p
     
+    # create array of type pairs
     types = np.arange(num_types, dtype=int)
     type_pairs = list(combinations(types, r=2))
     num_pairs = len(type_pairs)
     
+    # initialize arrays for least squares solving
     coefficient_matrix = np.zeros((num_pairs + 1, num_types))
     b = np.zeros(num_pairs + 1)
     
+    # populate arrays
     for index, (t1, t2) in enumerate(type_pairs):
         coefficient_matrix[index, t1] = 1.0
         coefficient_matrix[index, t2] = -1.0
         b[index] = np.mean(occupying_energies[:, t1] - occupying_energies[:, t2])
-        
+    
+    # populate last row with concentrations in chunk
     coefficient_matrix[num_pairs, :] = np.mean(init_types - 1 == types[:, None], axis=1)
     
+    # populate last member of b with the mean occupying energy-per-atom of the reference configuration
     reference_occupying_energies = occupying_energies[np.arange(occupying_energies.shape[0]), init_types - 1]
     b[num_pairs] = np.mean(reference_occupying_energies) / occupying_energies.shape[0]
     
+    # return least squares solution
     return np.linalg.lstsq(coefficient_matrix, b, rcond=None)[0]
 
 
 def main():
+
+    """
+    write chemical potentials to file
+    """
 
     axes = {
         'x': 0,
@@ -77,18 +125,24 @@ def main():
         'z': 2
     }
 
+    # load in data, sort by the column specified by axes
     data = np.loadtxt(sys.argv[1])
-    data = data[data[:, -1].argsort()]
+    data = data[data[:, -3 + axes[sys.argv[2]]].argsort()]
     z_coords = data[:, -3 + axes[sys.argv[2]]]
     num_atoms_per_chunk = int(sys.argv[3])
+    
+    # create windows with each chunk
     windows = sliding_window_view(z_coords, window_shape=num_atoms_per_chunk)
+    
+    # initialize and populate arrays with centers of each window and chemical potential in chunk
     window_centers = np.zeros(windows.shape[0])
     chemical_potentials = np.zeros((windows.shape[0], data.shape[1] - 4))
     for i, w in enumerate(windows):
         window_centers[i] = np.mean(w)
         filter_ = OneDimChunkFilter(axis=2, low=np.min(w), high=np.max(w))
         chemical_potentials[i, :] = get_chemical_potentials(data, filter_=filter_)
-        
+    
+    # create a header, save to text file
     header='center'
     for i in np.arange(data.shape[1] - 4, dtype=int):
         header += f' mu_{i + 1:.0f}'
--- chemical_potentials.sh
+++ chemical_potentials.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
+# load in config file from command line args
 config_file_name=$1
 
+# define a shorthand function to grab values from keys in configuration file
 cfg () {
 	jq -r $1 ${config_file_name}
 }
@@ -12,16 +14,20 @@ np=$(cfg '.np')
 pair_style=$(cfg '.pair_style')
 pair_coeff=$(cfg '.pair_coeff')
 
+# create potential file from specified pair style and pair coeff
 potential_file=$(cfg '.potential_file')
 echo "pair_style ${pair_style}" > ${potential_file}
 echo "pair_coeff ${pair_coeff}" >> ${potential_file}
 
 input_data_file=$(cfg '.input_data_file')
+
+# grab the number of types from the input data file
 ntypes=$(grep -oP '\d+(?=\s*atom types)' ${input_data_file})
 
 # using cfg() doesn't work here? why?
 mpi_args=$(jq -r '.mpi_args | join(" ")' ${config_file_name})
 
+# from envvars subdictionary, create a string that bash can read to define those environment variables
 envvars_dict=$(cfg '.env_vars')
 envvars=""
 for key in $(echo ${envvars_dict} | jq -r 'keys_unsorted[]')
@@ -34,12 +40,14 @@ options=$(cfg '.lmp_options')
 
 occupying_energies_file=$(cfg '.occupying_energies_file')
 
+# create string with variables
 var_str="-var ntypes ${ntypes} \
   -var potential_file ${potential_file} \
   -var input_data_file ${input_data_file} \
   -var occupying_energies_file ${occupying_energies_file} \
   -var relaxed_data_file $(cfg '.relaxed_data_file')"
 
+# add extra variables to string
 more_vars=("units" "dmax" "pressure" "vmax" "etol" "ftol" "maxsteps")
 vars_dict=$(cfg '.extra_vars')
 for var in "${more_vars[@]}"
@@ -47,8 +55,10 @@ do
   var_str+=" -var ${var} $(echo ${vars_dict} | jq --arg keyvar "$var" '.[$keyvar]')"
 done
 
+# call LAMMPS
 env ${envvars} mpirun ${mpi_args} -np ${np} ${lmp} ${options} -in in.insertions -log $(cfg '.log_file') ${var_str}
 
+# call analysis script
 python chemical_potentials.py ${occupying_energies_file} \
   $(cfg '.axis') \
   $(cfg '.num_atoms_per_chunk') \
